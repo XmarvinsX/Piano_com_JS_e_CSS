c8ee4fdde86f65bcc6db01bf8dc72efceaadf4c7
Marcelo Costa Campos <marcelovcgc@hotmail.com>
2024-12-02 18:46:45 -0300

    correção

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -3,7 +3,15 @@ const pianokeys = document.querySelectorAll(".piano-keys .key");
 let audio = new Audio("src/tunes/a.wav");
 
 const playtune = (key)=> {audio.src=`src/tunes/${key}.wav`; audio.play();
+ const clickedkey = document.querySelector(`[data-key="${key}"]`)
+clickedkey.classList.add("active");
+ setTimeout(()=>{
+     clickedkey.classList.remove("active");
+ },150);
 };
+
+
+//
 
 pianokeys.forEach((key)=> {
     console.log(key.dataset.key);
@@ -14,4 +22,4 @@ pianokeys.forEach((key)=> {
 document.addEventListener("keydown", (e)=>{
     playtune(e.key)
    
-})
+   })
